365ba3ec7b3dbe82944b1ddb6f8a154546be47e2
maccos <macos@maccoss-MacBook-Pro.local>
2025-12-26 16:14:31 +0530

    Fixed api endpoint in api.js

--- frontend/src/api/api.jsx
+++ frontend/src/api/api.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseURL = import.meta.env.VITE_API_BASE_URL;
+const baseURL = import.meta.env.VITE_API_BASE_URL + "/api/v1";
 const api = axios.create({
     baseURL: baseURL,
     withCredentials: true,
